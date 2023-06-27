e56c89158417bf1235e5b27405fc568859888c2a
urosjarc <jar.fmf@gmail.com>
2023-06-27 11:43:16 +0200

    Fixing domain map generation.

--- api/core/build.gradle.kts
+++ api/core/build.gradle.kts
@@ -10,15 +10,12 @@ dependencies {
     this.implementation("io.github.serpro69:kotlin-faker:1.14.0-rc.2")
 }
 
-data class Par(
+data class Parameter(
     val def: String, val ime: String, val tip: String, val rel: String?
 ) {
 
-    fun planUML(): String = "\t${this.ime}\n"
-    fun typescript(): String = "\t${this.ime}: string;\n"
-
     companion object {
-        fun parse(line: String): Par {
+        fun parse(line: String): Parameter {
             val info = line.trim().removeSuffix("override").trim().removeSuffix(",").split(":")
             val paramInfo = info.first().split(" ")
             var type = info.last()
@@ -31,22 +28,18 @@ data class Par(
                 rel = type.split("Id<").last().split(">").first()
                 rel = rel.split("<").first()
             }
-            return Par(def = paramInfo.first(), ime = paramInfo.last(), tip = type.trim(), rel = rel)
+            return Parameter(def = paramInfo.first(), ime = paramInfo.last(), tip = type.trim(), rel = rel)
         }
     }
 }
 
-data class Enu(
+data class Enumerator(
     val ime: String, val vrednosti: MutableList<String> = mutableListOf()
 ) {
-    fun plantUML() {
-
-    }
-
     companion object {
-        fun parse(line: String): Enu {
+        fun parse(line: String): Enumerator {
             val lineInfo = line.trim().removePrefix("enum class ").replace(",", "").split(" ")
-            val enum = Enu(ime = lineInfo.first())
+            val enum = Enumerator(ime = lineInfo.first())
             for (i in 2 until lineInfo.size) {
                 enum.vrednosti.add(lineInfo[i])
             }
@@ -55,102 +48,120 @@ data class Enu(
     }
 }
 
-data class Cls(
-    val ime: String = "",
-    val lastnosti: MutableList<Par> = mutableListOf(),
-    var enums: MutableList<Enu> = mutableListOf()
+data class DataClass(
+    val ime: String,
+    val file: File,
+    val lastnosti: MutableList<Parameter> = mutableListOf(),
+    var enums: MutableList<Enumerator> = mutableListOf()
+)
+
+data class Package(
+    val dataClasses: MutableList<DataClass>,
+    val file: File
 ) {
-    fun class_plantUML(includePar: Boolean): String {
-        var text = "class $ime {\n"
-        for (l in lastnosti) {
-            if (includePar) text += l.planUML()
-        }
-        text += "}\n"
-        return text
-    }
-
-    fun class_typescript(): String {
-        var text = "interface $ime {\n"
-        for (l in lastnosti) {
-            text += l.typescript()
-        }
-        text += "}\n"
-        return text
-    }
-    fun rel_plantUML(includeLabels: Boolean): String {
-        var rel = ""
-        for (l in lastnosti) {
-            if (l.rel != null) {
-                rel += "${this.ime} --> ${l.rel}"
-                rel += if (includeLabels) ": ${l.rel}\n" else "\n"
-            }
-        }
-        return rel
-    }
-
-
     companion object {
-        fun parseName(line: String): String = line.split(" ").last().removeSuffix("(")
-        fun parse(file: File): MutableList<Cls> {
-            val clses = mutableListOf<Cls>()
+        private fun parseClassName(line: String): String = line.split(" ").last().removeSuffix("(")
+        fun parse(file: File): Package {
+            val dataClasses = mutableListOf<DataClass>()
             for (line in file.readLines()) {
+
                 val cleanLine = line.trim()
-                println(cleanLine)
+
                 if (cleanLine.startsWith("data class")) {
-                    clses.add(Cls(ime = this.parseName(cleanLine)))
+                    dataClasses.add(DataClass(ime = this.parseClassName(cleanLine), file = file))
                     continue
                 }
 
-                if (clses.isEmpty()) continue
+                if (dataClasses.isEmpty()) continue
 
                 if (cleanLine.contains("val ") || cleanLine.contains("var ")) {
-                    clses.last().lastnosti.add(Par.parse(cleanLine))
+                    dataClasses.last().lastnosti.add(Parameter.parse(cleanLine))
                     continue
                 }
                 if (cleanLine.startsWith("enum class")) {
-                    clses.last().enums.add(Enu.parse(cleanLine))
+                    dataClasses.last().enums.add(Enumerator.parse(cleanLine))
                     continue
                 }
             }
-            return clses
+            return Package(dataClasses = dataClasses, file = file)
         }
     }
+}
+
+fun plantUML(packages: List<Package>): String {
+    val text = mutableListOf(
+        "@startuml",
+        "skinparam backgroundColor darkgray",
+        "skinparam ClassBackgroundColor lightgray\n"
+    )
+    val relations = mutableListOf<String>()
+    for (pac in packages) {
+        val insertPac = pac.dataClasses.size > 1
+        var indent = ""
+        if (insertPac) {
+            text.add("package ${pac.file.nameWithoutExtension}-KT <<Folder>> {\n")
+            indent = "\t"
+        }
+        for (dataClass in pac.dataClasses) {
+            text.add("${indent}class ${dataClass.ime} {")
+            for (lastnost in dataClass.lastnosti) {
+                text.add("${indent}\t${lastnost.ime}")
+                if (lastnost.rel != null) relations.add("${dataClass.ime} --> ${lastnost.rel}")
+            }
+            text.add("${indent}}\n")
+        }
+        if (insertPac) text.add("}\n")
+    }
+    text += relations
+    text.add("@enduml")
+    return text.joinToString(separator = "\n")
+}
+
+fun typescript(packages: List<Package>): String {
+    val text = mutableListOf<String>()
+    val response = mutableListOf("interface Response {")
+
+    //Relationship
+    //                      parent  child
+    val rel = mutableListOf<String, String>()
+    for (pac in packages) {
+        for (dataClass in pac.dataClasses) {
+            for (lastnost in dataClass.lastnosti) {
+                if(lastnost.ime.endsWith("_id")){
+                    val parent = lastnost.ime.replace("_id", "")
+                    val child = dataClass.ime
+                    rel[parent] = child
+                }
+            }
+        }
+    }
+    for (pac in packages) {
+        for (dataClass in pac.dataClasses) {
+            text.add("interface ${dataClass.ime} {")
+            response.add("\t${dataClass.ime.toLowerCase()}: {[key: string]: ${dataClass.ime}};")
+            for (lastnost in dataClass.lastnosti) {
+                text.add("\t${lastnost.ime}: string;")
+            }
+            //TODO: Build relationships here!
+            text.add("}\n")
+        }
+    }
+    response.add("}\n")
+    text += response
+    return text.joinToString(separator = "\n")
 }
 
 // Use the default greeting
 tasks.register<DefaultTask>("domainMap") {
     val root = this.project.projectDir
     val build = this.project.buildDir
-    val path = "src/main/kotlin/si/urosjarc/server/core/domain"
-    val domain = File(root, path)
+    val domain = File(root, "src/main/kotlin/si/urosjarc/server/core/domain")
+    val packages = mutableListOf<Package>()
 
-    var plantUML = """
-        @startuml
-        skinparam backgroundColor darkgray 
-        skinparam ClassBackgroundColor lightgray
-        
-        
-    """.trimIndent()
-
-    var typescript = ""
-    var typescript_linkedList = "interface LinkedList {\n"
-    var relations = ""
     domain.walkTopDown().forEach {
-        if (it.isFile) {
-            val cls = Cls.parse(it)
-            if (cls.size > 1) plantUML += "package ${it.nameWithoutExtension}-KT <<Folder>> {\n\n"
-            for (c in cls) {
-                plantUML += c.class_plantUML(includePar = true)
-                relations += c.rel_plantUML(includeLabels = false)
-                typescript += c.class_typescript()
-                typescript_linkedList += "\t${c.ime.toLowerCase()}: {[key: string]: ${c.ime}};\n"
-            }
-            if (cls.size > 1) plantUML += "\n}\n\n"
-        }
+        if (it.isFile) packages.add(Package.parse(it))
     }
-    typescript_linkedList += "\n}\n\n"
-    plantUML += "\n$relations\n@enduml"
 
-    File(build, "domain.plantuml").writeText(plantUML)
-    File(build, "domain.ts").writeText(typescript + typescript_linkedList)
+    File(build, "domain2.plantuml").writeText(plantUML(packages))
+    File(build, "domain2.ts").writeText(typescript(packages))
 }
