4e6aad79c5b47e138a04e8d1c16cfcb4fa89e235
urosjarc <jar.fmf@gmail.com>
2023-07-27 01:57:04 +0200

    Move to app2

--- app/src/lib/extends/Duration.ts
+++
@@ -1,9 +0,0 @@
-import * as Duration from "iso8601-duration"
-
-export function Duration_vSekunde(duration: Duration.Duration) {
-  return Duration.toSeconds(duration)
-}
-
-export function Duration_vMinute(duration: Duration.Duration) {
-  return Duration_vSekunde(duration) / 60
-}
