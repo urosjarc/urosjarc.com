bb7e76e8d4879255363cf4095a8e32aa2a118072
urosjarc <jar.fmf@gmail.com>
2023-07-27 10:37:30 +0200

    Posodobobitev vsega.

--- app2/src/lib/usecases/poslji_kontakt.ts
+++
@@ -1,6 +0,0 @@
-import {API} from "../stores/apiStore";
-import type {KontaktObrazecReq} from "$lib/api";
-
-export async function poslji_kontakt(args: KontaktObrazecReq) {
-  return API().postKontakt(args);
-}
