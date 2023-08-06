ebeeac4683b78817b10931492dadf7d8b15c62e4
urosjarc <jar.fmf@gmail.com>
2023-08-06 17:39:51 +0200

    Protecting db sitting data!.

--- api/core/src/main/kotlin/base/Encrypted.kt
+++ api/core/src/main/kotlin/base/Encrypted.kt
@@ -3,7 +3,6 @@ package base
 import kotlinx.serialization.Serializable
 import org.springframework.security.crypto.encrypt.Encryptors
 import org.springframework.security.crypto.encrypt.TextEncryptor
-import kotlin.system.measureNanoTime
 
 val encryptor: TextEncryptor = Encryptors.delux(Env.ENCRYPTION_PASSWORD, Env.ENCRYPTION_SALT)
 
@@ -19,11 +18,4 @@ class Encrypted {
         return encryptor.decrypt(String(this.encrypted_bytes))
     }
 
-}
-
-
-fun main() {
-    val e = Encrypted("test")
-    var test = e.decrypt()
-    print(test)
 }
