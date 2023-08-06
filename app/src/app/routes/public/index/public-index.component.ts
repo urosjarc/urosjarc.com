348387f644eef9592dc62ef6d3d1509a2cafc881
urosjarc <jar.fmf@gmail.com>
2023-08-06 12:52:12 +0200

    Adding text.

--- app/src/app/routes/public/index/public-index.component.ts
+++ app/src/app/routes/public/index/public-index.component.ts
@@ -22,20 +22,38 @@ export class PublicIndexComponent {
   ]
   infos: { naslov: string, vsebina: string }[] = [
     {
-      naslov: "Izvajam kvalitetne inštrukcije programiranja, fizike, matematike.",
-      vsebina: "Izvajam kvalitetne inštrukcije programiranja, fizike, matematike."
+      naslov: "Kvalitetne inštrukcije programiranja, fizike, matematike.",
+      vsebina: `
+        Inštrukcije gimnazijske matematike in fizike, ter programiranja na univerzitetnem nivoju.
+        Priprava učencev na popravni izpit matematike in fizike. Dolgotrajna pomoč pri učenju programiranja za potrebe
+        strokovnih srednjih šol pri programskih jezikih (C, C++, Java, Javascript, Node, Python, Kotlin).
+      `
     },
     {
-      naslov: "Pripravljam študente vseh fakultet na izpite iz programiranja.",
-      vsebina: "Pripravljam študente vseh fakultet na izpite iz programiranja."
+      naslov: "Priprava študente vseh fakultet na izpite iz programiranja.",
+      vsebina: `
+        Inštrukcije študentom FRI, FERI, FE, pri predmetih C, Java, Python, Node.js ter razvoju spletnih aplikacij.
+        Pomoč pri razumevanju težavnih aspektov programiranja za napredne podatkovne strukture ter
+        razvoj in razumevanje algoritmov ki se izvajajo na omenjenih strukturah.
+        Pridobivanje globokega razumevanja o skritih stvari, ki jih računalnik opravlja v ozadju med izvajanjem
+        programa.
+      `
     },
     {
-      naslov: "Pomagam pri programiranju diplomskih in mag. nalog.",
-      vsebina: "Pomagam pri programiranju diplomskih in mag. nalog."
+      naslov: "Pomagam pri programiranju projektnih, diplomskih ter mag. nalog.",
+      vsebina: `
+        Podatkovna analitika obsežnih podatkov ter njihova statistična obdelava.
+        Priprava rezultatov analize za javno objavo v publikaciji ter javnih medijih.
+        Razvoj in svetovanje pri razvoju modelov strojnega učenja, spletnih strežnikov in aplikacij.
+      `
     },
     {
-      naslov: "Pomagam pri prekvalifikaciji za programersko službo",
-      vsebina: "Pomagam pri prekvalifikaciji za programersko službo"
+      naslov: "Mentoriranje pri 4 letni dolgi prekvalifikaciji za programersko pozicijo.",
+      vsebina: `
+        Priprava 4 letnega programa za prekvalifikacijski postopek ter pripravo za željeno pozicijo.
+        Čustvena podpora pri težkem in dolgotrajnem postopku. Pomoč pri iskanju primerne
+        pozicije ob koncu učenja.
+      `
     },
   ];
 }
