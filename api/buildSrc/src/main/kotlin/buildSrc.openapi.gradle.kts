4728722a0a1d227529141d798e35bb48354b63b7
urosjarc <jar.fmf@gmail.com>
2023-08-15 14:10:52 +0200

    Finished openapi generation.

--- api/buildSrc/src/main/kotlin/buildSrc.openapi.gradle.kts
+++ api/buildSrc/src/main/kotlin/buildSrc.openapi.gradle.kts
@@ -16,14 +16,14 @@ class OpenApi : Plugin<Project> {
                 val outputDir = File(this.project.projectDir, extension.outputDir.get())
                 val inputDir = File(this.project.projectDir, extension.inputDir.get())
                 logger.warn("\nOutput dir: $outputDir\n")
-//                inputDir.listFiles { it.isFile && it.extension == "yaml" }.forEach {
-//                    val content = it.readText()
-//                        .replace("'*/*':", "'application/json':")
-//                        .replace("https://server", "http://0.0.0.0:8080")
-//                    val distFile = "${outputDir.absolutePath}/${it.name}"
-//                    logger.warn("CREATING: $distFile")
-//                    file(distFile).writeText(content)
-//                }
+                inputDir.listFiles { file -> file.isFile && file.extension == "yaml" }?.forEach {
+                    val content = it.readText()
+                        .replace("'*/*':", "'application/json':")
+                        .replace("https://server", "http://0.0.0.0:8080")
+                    val distFile = "${outputDir.absolutePath}/${it.name}"
+                    logger.warn("CREATING: $distFile")
+                    File(distFile).writeText(content)
+                }
             }
         }
     }
