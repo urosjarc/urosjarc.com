4df009143742d49dd4e88fa8711694fa988f2827
urosjarc <jar.fmf@gmail.com>
2023-08-30 00:40:25 +0200

    Adding some more stuff.

--- data/src/omega.py
+++ data/src/omega.py
@@ -6,13 +6,30 @@ from PIL import Image
 
 from src import utils
 
+def rdeci_pixel(r,g,b):
+    pass
 
-def procesiraj_sliko(imgs):
-    for img in imgs:
+def razrezi_sliko(img: Image.Image):
+    deli_slike = [[]]
+    for y in range(img.height):
+        nova_naloga = False
+        for x in range(img.width / 5):
+            pixel = img.getpixel((x, y))
+            if pixel:
+                pass
+
+
+def slika(img: Image.Image):
+    for del_slike in razrezi_sliko(img):
         print(utils.image_to_text(img))
 
 
-def procesiraj_omego():
+def slike(imgs: list[Image.Image]):
+    for img in imgs:
+        slika(img)
+
+
+def zip():
     omegaPath = Path("../omega")
     files = omegaPath.iterdir()
 
@@ -30,15 +47,16 @@ def procesiraj_omego():
                 continue
 
             img = Image.open(ifile)
+            hsv_img = img.convert('HSV')
             imgs.append(img)
 
             img.show()
             break
 
-    utils.parallel(procesiraj_sliko, 5, imgs)
+    utils.parallel(slike, 5, imgs)
 
 
 start = time.time()
-procesiraj_omego()
+zip()
 end = time.time()
 print(end - start)
