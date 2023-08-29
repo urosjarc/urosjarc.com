8b359c9fd4a54dabe22ef933f84134570ac892d6
urosjarc <jar.fmf@gmail.com>
2023-08-29 05:45:43 +0200

    Poenostavitev kode.

--- data/src/omega.py
+++ data/src/omega.py
@@ -1,47 +1,47 @@
-import datetime
 import time
 import zipfile
 from multiprocessing import Process
 from pathlib import Path
 
 from PIL import Image
-import PIL
 
 
-def procesiraj_sliko(img: Image.Image):
+def parallel(fun, args):
+    pool = []
+
+    for arg in args:
+        p = Process(target=fun, args=(arg,))
+        p.start()
+        pool.append(p)
+
+    for p in pool:
+        p.join()
+
+
+def procesiraj_sliko(img):
     a = 0
     for y in range(img.height):
         for x in range(img.width):
             a += 1
-    print(a)
-    img.save(f"{datetime.datetime.now()}.png")
-
-
-def procesiraj_zip(file):
-    zip = zipfile.ZipFile(file)
-    zip_slike = zip.infolist()
-
-    procs = []
-    for zip_slika in zip_slike:
-        slika = Image.open(zip.open(zip_slika))
-        p = Process(target=procesiraj_sliko, args=(slika,))
-        p.start()
-        procs.append(p)
-    [p.join() for p in procs]
 
 
 def procesiraj_omego():
     omegaPath = Path("../omega")
     files = omegaPath.iterdir()
 
-    procs = []
+    imgs = []
+
     for file in files:
-        p = Process(target=procesiraj_zip, args=(file,))
-        p.start()
-        procs.append(p)
-    [p.join() for p in procs]
+        zip = zipfile.ZipFile(file)
+        zip_slike = zip.infolist()
+        for zip_slika in zip_slike:
+            ifile = zip.open(zip_slika)
+            imgs.append(Image.open(ifile))
+
+    parallel(procesiraj_sliko, imgs)
+
 
 start = time.time()
 procesiraj_omego()
 end = time.time()
-print(end-start)
+print(end - start)
