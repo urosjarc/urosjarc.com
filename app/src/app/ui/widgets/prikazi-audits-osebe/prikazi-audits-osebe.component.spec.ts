105f59428ee98c059836b64b3795f240a7e54185
Ethernal12 <korbar41@email.com>
2023-09-16 17:03:41 +0200

    Dodan test prikazi-audits-osebe.component.spec.ts.

--- app/src/app/ui/widgets/prikazi-audits-osebe/prikazi-audits-osebe.component.spec.ts
+++ app/src/app/ui/widgets/prikazi-audits-osebe/prikazi-audits-osebe.component.spec.ts
@@ -4,12 +4,12 @@ import {DatePipe} from "@angular/common";
 import {DateOddaljenostPipe} from "../../pipes/date-oddaljenost/date-oddaljenost.pipe";
 import {DurationPipe} from "../../pipes/duration/duration.pipe";
 import {OsebaRepoService} from "../../../core/repos/oseba/oseba-repo.service";
-import SpyObj = jasmine.SpyObj;
 import {DbService} from "../../../core/services/db/db.service";
 import {NoopAnimationsModule} from "@angular/platform-browser/animations";
+import {ActivatedRoute} from "@angular/router";
 
 describe('widgets: prikazi-audits-osebe', () => {
-  let fixture : ComponentFixture<PrikaziAuditsOsebeComponent>;
+  let fixture: ComponentFixture<PrikaziAuditsOsebeComponent>;
   let component: PrikaziAuditsOsebeComponent;
   let spyOnOsebaRepo;
   beforeEach(() => {
@@ -18,19 +18,22 @@ describe('widgets: prikazi-audits-osebe', () => {
         NoopAnimationsModule
       ],
 
-    providers: [
-      DatePipe,
-      DateOddaljenostPipe,
-      DurationPipe,
-      OsebaRepoService,
-      DbService,
+      providers: [
+        DatePipe,
+        DateOddaljenostPipe,
+        DurationPipe,
+        OsebaRepoService,
+        DbService,
 
-    ]
+        {provide: ActivatedRoute, useValue:'/'}
+
+      ]
 
     }).compileComponents()
 
     fixture = TestBed.createComponent(PrikaziAuditsOsebeComponent);
     component = fixture.componentInstance;
+    TestBed.inject(OsebaRepoService);
   })
 
   it('mora inicializirati komponento', () => {
@@ -39,14 +42,32 @@ describe('widgets: prikazi-audits-osebe', () => {
 
   it('mora pravilno klicati funkicje ob ngOnInit()', async () => {
     spyOnOsebaRepo = spyOn((component as any).osebaRepo, 'audits').and.callThrough();
-    console.log(spyOnOsebaRepo, 'spy--------------------')
-    component.ngOnInit();
     fixture.detectChanges();
     expect(spyOnOsebaRepo).toHaveBeenCalled()
   });
-  //TODO: NE VEM ČE TO FUNKCIJONALNOST TESTIRATI
-  it('mora pravilno napolniti models array', () => {
-      expect(true).toBeTrue()
+  //TODO: TUKAJ SEM NAMESTO ustvarjeno: '2023-09-16T10:00:00', NAPOLNIL Z {ustvarjeno: new Date(2023-09-16T10:00:00)}, in se component.audits.data ni napolnil, brez vrnjenega errorja ?
+  it('mora pravilno napolniti audits.data array', async () => {
+    const mockAudits = [
+      {
+        tip: 'testni tip 1',
+        opis: 'testni opis 1',
+        trajanje: '1000',
+        ustvarjeno: '2023-09-16T10:00:00',
+        'Pred...': '2023-09-16T10:00:00'
+      },
+      {
+        tip: 'testni tip 2',
+        opis: 'testni opis 2',
+        trajanje: '2000',
+        ustvarjeno: '2023-09-16T10:00:00',
+        'Pred...': '2023-09-16T10:00:00'
+      }
+    ];
+    spyOn((component as any).osebaRepo, 'audits').and.returnValue(Promise.resolve(mockAudits))
+    fixture.detectChanges();
+    await fixture.whenStable();
+
+    expect(component.audits.data.length).toEqual(2);
   });
 
 })
