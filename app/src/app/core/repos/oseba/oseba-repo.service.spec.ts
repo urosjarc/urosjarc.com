19bfff1e5a94335b1d2e3a606f737ee0865b7826
Ethernal12 <korbar41@email.com>
2023-09-18 18:30:09 +0200

    Zmokan db v testih: oseba-repo.service.spec.ts.

--- app/src/app/core/repos/oseba/oseba-repo.service.spec.ts
+++ app/src/app/core/repos/oseba/oseba-repo.service.spec.ts
@@ -2,32 +2,81 @@ import {OsebaRepoService} from "./oseba-repo.service";
 import {TestBed} from "@angular/core/testing";
 import {DbService} from "../../services/db/db.service";
 
-describe('repos: oseba-repo test', () => {
-    let osebaRepoService: OsebaRepoService;
-
-    beforeEach(() => {
-
-
-        TestBed.configureTestingModule({
-            providers: [
-                OsebaRepoService,
-                DbService
-            ]
-        })
-        TestBed.inject(OsebaRepoService)
+function helperMockFunkcija(data: string | object){
+  return {
+    where: jasmine.createSpy('where').and.returnValue({
+      equals: jasmine.createSpy('equals').and.returnValue({
+        first: jasmine.createSpy('first').and.returnValue(
+          Promise.resolve(data)
+        ),
+        toString: jasmine.createSpy('toString').and.returnValue(data),
+        toArray: jasmine.createSpy('toArray').and.returnValue(Promise.resolve(data))
+      })
     })
+  }
+}
 
-    it('oseba() mora vrniti objekt z pravilnimi podatki', async () => {
-        const mockProfilId = '123';
-        const mockOsebaData = {
-            oseba: { _id: '123'},
-            naslovi: [{ oseba_id: '123'}],
-            kontakti: [{ oseba_id: '123'}],
-        };
+describe('repos: oseba-repo test', () => {
+  let osebaRepoService: OsebaRepoService;
 
-        spyOn(DbService.prototype, 'get_profil_id').and.returnValue(mockProfilId);
-        await osebaRepoService.oseba()
-        expect(true).toBeTrue()
+  beforeEach(() => {
+    Promise.resolve([{
+      zvezek: 'zvezek',
+      tematike: 'tematike'
+    }])
 
-    });
+    const mockDbService = {
+      get_profil_id: jasmine.createSpy('get_profil_id').and.returnValue('1234'),
+
+      zvezek: {
+          toArray: jasmine.createSpy('toArray').and.returnValue(
+            Promise.resolve([{
+              _id: '12121212',
+              zvezek: 'zvezek',
+              tematike: 'tematike'
+            }])
+          ),
+        },
+      oseba: helperMockFunkcija('1234'),
+      naslov: helperMockFunkcija('naslovi_id'),
+      kontakt: helperMockFunkcija('kontakti_id'),
+      sporocilo: helperMockFunkcija([{'kontakt_posiljatelj_id' : '12344', 'poslano': '2023-8-19' }]),
+      // tematika: {
+      //   where: jasmine.createSpy('where').and.returnValue({
+      //     equals: jasmine.createSpy('equals').and.returnValue({
+      //       toString: jasmine.createSpy('toString').and.returnValue('mocked_value'),
+      //       toArray: jasmine.createSpy('toArray').and.returnValue(Promise.resolve([{_id: 'nekiid'}])
+      //       ),
+      //     }),
+      //   }),
+      // },
+      tematika: helperMockFunkcija([{_id: 'nekiid'}]),
+      naloga: helperMockFunkcija('naloga')
+    };
+    TestBed.configureTestingModule({
+      providers: [
+        OsebaRepoService,
+        {provide: DbService, useValue: mockDbService}
+      ]
+    })
+    osebaRepoService = TestBed.inject(OsebaRepoService);
+  })
+
+  it('oseba() mora vrniti objekt z pravilnimi podatki', async () => {
+    const result = await osebaRepoService.oseba()
+    expect(typeof result?.oseba).toEqual('string')
+    expect(typeof result?.kontakti).toEqual('string')
+    expect(typeof result?.naslovi).toEqual('string')
+  });
+
+  it('sporocila() mora vrniti vsa sporocila', async () => {
+    const result = await osebaRepoService.sporocila()
+    expect(true).toBeTrue()
+
+  });
+  it('zvezki() mora vrniti vse zvezke', async() => {
+    const result = await osebaRepoService.zvezki();
+    console.log(result, 'zvezki---------------------------')
+    expect(true).toBeTrue()
+  });
 })
