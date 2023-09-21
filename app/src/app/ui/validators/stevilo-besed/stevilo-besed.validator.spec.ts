25b5c286c7219d6d83c34c118946f0b03b0183c8
Ethernal12 <korbar41@email.com>
2023-09-21 16:15:26 +0200

    Refactor število-besed validator testi in dodani testi za edge case.

--- app/src/app/ui/validators/stevilo-besed/stevilo-besed.validator.spec.ts
+++ app/src/app/ui/validators/stevilo-besed/stevilo-besed.validator.spec.ts
@@ -2,22 +2,28 @@ import {Validator_stevilo_besed} from "./stevilo-besede.validator";
 import {FormControl, ValidationErrors} from "@angular/forms";
 
 describe('Validator: stevilo-besed test', () => {
+  const validator = Validator_stevilo_besed(3);
+  const control = (besediloMsg:  string) => {return new FormControl(besediloMsg);}
   it('mora vrniti error msg, če je prisotnih premalo besed', () => {
-    const validator = Validator_stevilo_besed(3);
-    const control = new FormControl('Premalo besed');
-
-    const result: ValidationErrors | null = validator(control);
+    const result: ValidationErrors | null = validator(control('Premalo besed'));
     const errorSporocilo = result && result['Validator_besed'];
-    expect(typeof errorSporocilo).toEqual('string')
+    expect(typeof errorSporocilo).toEqual('string');
   });
 
-  it('mora vrniti error msg, če je prisotnih premalo besed', () => {
-    const validator = Validator_stevilo_besed(3);
-    const control = new FormControl('Pravilni format sporocila');
-
-    const result: ValidationErrors | null = validator(control);
+  it('mora vrniti null, če sporočilo vsebuje zadostno število besed', () => {
+    const result: ValidationErrors | null = validator(control('Pravilni format sporocila'));
     const errorSporocilo = result && result['Validator_besed'];
-    expect(errorSporocilo).toBeNull()
+    expect(errorSporocilo).toBeNull();
   });
+  it('edge case test1', () => {
+    const result: ValidationErrors | null = validator(control(' '));
+    const errorSporocilo = result && result['Validator_besed'];
+    expect(typeof errorSporocilo).toEqual('string');
+  })
+  it('edge case test2', () => {
+    const result: ValidationErrors | null = validator(control('%&#""#%'));
+    const errorSporocilo = result && result['Validator_besed'];
+    expect(typeof errorSporocilo).toEqual('string');
+  })
 
 })
