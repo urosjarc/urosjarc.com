d2f3c8a2b06d69343094d4f1660f6f708427749c
Ethernal12 <korbar41@email.com>
2023-10-05 17:36:53 +0200

    izberi-tip-osebe.component.spec.ts testa ni mi uspelo narediti, zaradi CORS napake.

---
+++ app/src/app/ui/windows/izberi-tip-osebe/izberi-tip-osebe.component.spec.ts
@@ -0,0 +1,47 @@
+//TODO: TUKAJ DOBIM CORS NAPAKO PRI INICILIAZAICJI KOMPONENTE
+
+// import {TestBed} from "@angular/core/testing";
+//
+// import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+// import {IzberiTipOsebeComponent} from "./izberi-tip-osebe.component";
+//
+// describe('IzberiTipOsebeComponent', () => {
+//   let component: IzberiTipOsebeComponent;
+//
+//   const mockCallback = jasmine.createSpy('callback');
+//
+//   const mockData = {
+//     model: {
+//       tipi: ['tip1', 'tip2', 'tip3'],
+//       callback: mockCallback,
+//     },
+//   };
+//
+//   beforeEach(() => {
+//     TestBed.configureTestingModule({
+//       providers:[
+//         {
+//           provide: MAT_DIALOG_DATA,
+//           useValue: {},
+//         },
+//       ]
+//
+//     });
+//
+//     const fixture = TestBed.createComponent(IzberiTipOsebeComponent);
+//     component = fixture.componentInstance;
+//   });
+//
+//   it('mora inicializirati komponento', () => {
+//     expect(component).toBeTruthy();
+//   });
+//   //TODO: to bi se morda lahko testiralo z cypress?
+//   it('mora ustrezno prepoznati click() event', () => {
+//    expect(true).toBeTrue();
+//   });
+// });
+
+
+
+
+
