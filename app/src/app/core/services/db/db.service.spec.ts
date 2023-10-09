e94c7ecdcf9dd5ea457c46e2322d913db6ca125d
Ethernal12 <korbar41@email.com>
2023-10-09 13:43:45 +0200

    Initial commit.

--- app/src/app/core/services/db/db.service.spec.ts
+++ app/src/app/core/services/db/db.service.spec.ts
@@ -1,7 +1,7 @@
 import {TestBed} from '@angular/core/testing';
 import {DbService} from './db.service';
 import {ucenecData} from "./db.service.spec.model";
-
+// initial commit
 describe('DbService tesitranje', () => {
   let dbService: DbService;
 
