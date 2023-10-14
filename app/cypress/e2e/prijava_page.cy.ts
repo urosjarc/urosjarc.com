1fe7704cb0a664a89988d6690e0ac2f37c896bb1
Ethernal12 <korbar41@email.com>
2023-10-14 17:07:44 +0200

    Prijava page tests.

---
+++ app/cypress/e2e/prijava_page.cy.ts
@@ -0,0 +1,68 @@
+import 'cypress-xpath';
+describe('Prijava page urosjarc.com tests', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:4200/prijava')
+  })
+
+  it('should get a error message if trying to log in on empty inputs field', () => {
+    cy.get('#mat-input-0').focus()
+    cy.get('#mat-input-1').focus()
+    cy.get('body').click();
+    cy.get('#mat-mdc-error-0').should('have.text', 'Vnos je obvezen!');
+    cy.get('#mat-mdc-error-1').should('have.text', 'Geslo je obvezno!');
+    cy.xpath('/html/body/app/app-public/app-card-navigacija/div/div/div/div/app-public-prijava/form/div/div[3]/button').click()
+    cy.get('#mat-mdc-dialog-title-0').should('have.text', ' KRITIČNA NAPAKA ')
+    // if clikced on zapri on error message the message should not be visible
+    cy.get('.mdc-dialog__container button').click()
+    cy.get('.mdc-dialog__container button').should('not.be.visible')
+  });
+  it('should log in ucenec page with correct credentials', () => {
+    cy.get('#mat-input-0').type('ucenec', { force: true })
+    cy.get('#mat-input-1').type('geslo', { force: true })
+    //click prijava
+    cy.xpath('/html/body/app/app-public/app-card-navigacija/div/div/div/div/app-public-prijava/form/div/div[3]/button').click()
+    cy.wait(5000);
+    cy.url().should('include', '/ucenec');
+    //click on nazaj
+    cy.xpath('/html/body/app/app-ucenec/app-card-navigacija/div/div/div/app-toolbar-navigacija/div/div[1]/app-button-toolbar/div/div[1]/button').click()
+    //click  prijava
+    cy.xpath('/html/body/app/app-public/app-card-navigacija/div/div/div/app-toolbar-navigacija/div/div[4]/app-button-toolbar/div/div[1]/button').click()
+    cy.url().should('include', '/ucenec');
+    //odjavi
+    cy.xpath('/html/body/app/app-ucenec/app-card-navigacija/div/div/div/app-toolbar-navigacija/div/div[5]/app-button-toolbar/div/div[1]/button').click()
+    cy.url().should('include', '/prijava');
+
+
+  });
+  it('should log in ucitelj page with correct credentials', () => {
+    cy.get('#mat-input-0').type('ucitelj', { force: true })
+    cy.get('#mat-input-1').type('geslo', { force: true })
+    //click prijava
+    cy.xpath('/html/body/app/app-public/app-card-navigacija/div/div/div/div/app-public-prijava/form/div/div[3]/button').click()
+    cy.wait(5000);
+    cy.intercept('GET', 'http://127.0.0.1:8080/auth/profil').as('interceptedRequest'); // Alias the intercepted request
+    //intercept response
+    cy.wait('@interceptedRequest').then(({ response }) => {
+      console.error(response, 'responseeeee//////////////*')
+      expect(response!.body._id).not.equal('undefined')
+    });
+    cy.url().should('include', '/ucitelj');
+    //click on nazaj
+    cy.xpath('/html/body/app/app-ucitelj/app-card-navigacija/div/div/div/app-toolbar-navigacija/div/div[1]/app-button-toolbar/div/div[1]/button').click()
+    //click  prijava
+    cy.xpath('/html/body/app/app-public/app-card-navigacija/div/div/div/app-toolbar-navigacija/div/div[4]/app-button-toolbar/div/div[1]/button').click()
+    cy.url().should('include', '/ucitelj');
+    //odjavi
+    cy.xpath('/html/body/app/app-ucitelj/app-card-navigacija/div/div/div/app-toolbar-navigacija/div/div[7]/app-button-toolbar/div/div[1]/button').click()
+    cy.url().should('include', '/prijava');
+  })
+  it('should not be able to force url / guards test', () => {
+    cy.visit('http://localhost:4200/ucenec')
+    //should redirect to prijava page
+    cy.url().should('include', '/prijava')
+    cy.visit('http://localhost:4200/ucitelj')
+    //should redirect to prijava page
+    cy.url().should('include', '/prijava')
+  });
+
+})
